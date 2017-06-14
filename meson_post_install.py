7d1778360d583b2ab4d2d28b4300d2174c9f6ef0
Patrick Griffis <tingping@tingping.se>
2017-06-14 18:52:55 -0400

    fixup! build: Replace Autotools with Meson

--- meson_post_install.py
+++ meson_post_install.py
@@ -14,5 +14,8 @@ if not destdir:
     for theme in ('hicolor', 'ubuntu-mono-dark', 'ubuntu-mono-light'):
         call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', theme)])
 
+    print('Compiling GSettings schemas...')
+    call(['glib-compile-schemas', path.join(datadir, 'glib-2.0', 'schemas')])
+
 print('Compiling python bytecode...')
 compile_dir(destdir + path.join(datadir, 'pithos', 'pithos'), optimize=2)
